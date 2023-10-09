0542dc3b5b67999fdc1cea1d0255a78374ea372f
nikolav <admin@nikolav.rs>
2023-10-09 21:19:02 +0100

    build:Mon Oct  9 20:19:02 UTC 2023

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,11 @@
 # run/build containers
 docker compose up -d
 
-# init database
-docker exec -it api python3 ./auto__db_setup.py
-
 # build ui
 docker exec -it app yarn
 docker exec -it app yarn build
 
+# init database
+docker exec -it api python3 ./auto__db_setup.py
 
 
