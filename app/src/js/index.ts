74598eb4f4084e58b0ed4f295f399fef63b5cc8a
nikolav <admin@nikolav.rs>
2023-10-15 07:46:43 +0100

    + app.src.scss.icons

--- app/src/js/index.ts
+++ app/src/js/index.ts
@@ -1,20 +1,24 @@
-import $ from "jquery";
-import "bootstrap";
 import axios from "axios";
+import $ from "jquery";
+import * as $$ from "bootstrap";
 
+import { API_URL_DOCS } from "../config/vars";
+
+const TOPIC = "@1";
 
 $(() => {
   console.log(`@ready [${Date.now()}]`);
+  console.log({ $$ });
 
   const btn$ = $("#B__ok");
 
-
   btn$.on({
     click: async (evt) => {
-      const { data } = await axios({
-        method: "get",
-        url: "https://jsonplaceholder.typicode.com/posts/22",
-      });
+      const { data } = await axios(
+        `${API_URL_DOCS}?tag=${encodeURIComponent(TOPIC)}`
+        // `${API_URL_DOCS}/23`
+      );
+
       alert(JSON.stringify(data));
     },
   });
